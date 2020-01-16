188abe0dbb8fdd302d1a9658525365c093a98a13
Gizmosoft <kartikey.hebbar@gmail.com>
2020-01-16 15:54:41 +0530

    function call & chaining

--- websockets_ex1_client.py
+++ websockets_ex1_client.py
@@ -6,14 +6,30 @@ import websockets
 async def hello():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        name = input("Hello! How may I help you? ")
+        greet = input("Bot> Hello! Would you want to report any cybercrime? ") #first question, answer in YES/NO
+        if(greet == "Yes"):
+            await websocket.send(greet)
+            print(f"User> {greet}")         #send the response
 
-        await websocket.send(name)
-        print(f"> {name}")
+            response = await websocket.recv()       #recv response from bot
+            print(f"Bot> {response}")
+            list = await websocket.recv()               #recv the list of frauds. This has to be links in rt
+            print(f"Bot> {list}")
+            test = await websocket.recv()
+            print(f"Bot> {test}")
+            next_var = await websocket.recv()
+            print(f"Bot> {next_var}")
+            test2 = await websocket.recv()
+            print(f"Bot> {test2}")
+        else:
+            await websocket.send(greet)
+            print(f"User> {greet}")             #send the response
+            goodbye = await websocket.recv()    #recv the goodbye message
+            print(f"Bot> {goodbye}")
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+        # greeting = await websocket.recv()
+        # print(f"< {greeting}")
 
-while(1):
+while True:
     asyncio.get_event_loop().run_until_complete(hello())
 # asyncio.get_event_loop().run_forever()
--- websockets_ex1_server.py
+++ websockets_ex1_server.py
@@ -4,16 +4,46 @@
 import asyncio
 import websockets
 
-async def jello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+goodbye = f"You can contact me anytime you want. Thank you!!"
+async def hello(websocket, path):
+    greet = await websocket.recv()              #receive YES/NO from user
+    print(f"Bot> Hello! Would you want to report any cybercrime?")
+    print(f"User> {greet}")
 
-    greeting = f"Hello {name}!"
+    if(greet == "Yes"):
+        response = f"Please select a cybercrime you want to report from the below list.\n"
+        await websocket.send(response)      #send the above statement
+        print(f"Bot> {response}")
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+        list = f"Lottery Scam\nJob Fraud\nPhishing\nMatrimony Scam\nOTP Fraud\n"
+        await websocket.send(list)      #send the list
+        print(f"Bot> {list}")
 
-start_server = websockets.serve(jello, "localhost", 8765)
+        #just for testing purpose####
+        test = f"\nSending you to a new function"
+        await websocket.send(test)
+        print(f"Bot> {test}")
+        await next(websocket, path)
+
+        test2 = f"\nBack to hello Function"
+        await websocket.send(test2)
+        print(f"Bot> {test2}")
+    else:
+        await websocket.send(goodbye)
+        print(f"Bot> {goodbye}")            #send goodbye msg in case of a NO
+        
+
+    # greeting = f"Hello {name}!"
+    #
+    # await websocket.send(greeting)
+    # print(f"> {greeting}")
+
+async def next(websocket, path):
+    next_var = f"\nYou are in the new function!"
+    await websocket.send(next_var)
+    print(f"Bot> {next_var}")
+
+start_server = websockets.serve(hello, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
