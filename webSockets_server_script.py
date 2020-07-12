bb4369fd92734ddda6ec3f1d79bb7033c263f3a4
Gizmosoft <kartikey.hebbar@gmail.com>
2020-07-12 11:39:57 +0530

    mini changes

--- webSockets_server_script.py
+++ webSockets_server_script.py
@@ -12,7 +12,19 @@ async def time(websocket, path):
         await websocket.send(now)
         await asyncio.sleep(random.random() * 3)
 
-start_server = websockets.serve(time, "127.0.0.1", 5678)
+async def welcome(websocket, path):
+    WELCOME_INTENT = ["Hello! I am CyberEye. How may I help you?\n",
+                      "Hi! I am CyberEye. How may I help you?\n",
+                      "Hey! I am CyberEye. How may I help you?\n"]
+    wel = random.choice(WELCOME_INTENT)
+    await websocket.send(wel)
+    return next_method()
+
+async def next_method(websocket):
+    next = "Here we are in next function!"
+    await websocket.send(next)
+
+start_server = websockets.serve(welcome, "127.0.0.1", 8000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
